ddaeca21c128bbefd4a543cad0c74f39d98ee19c
Pablo Bellido <bellido.peru26@gmail.com>
2024-09-04 21:14:04 -0500

    first commit

---
+++ src/test/java/com/dh/reservation_system/ReservationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dh.reservation_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservationSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
