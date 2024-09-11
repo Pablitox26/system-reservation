a4650e5b31dc24a9904faae38d60456d48b53a89
Pablo Bellido <bellido.peru26@gmail.com>
2024-09-10 22:36:01 -0500

    feat(logger): add logger for dentist service

--- src/main/java/com/dh/reservation_system/services/impl/DentistService.java
+++ src/main/java/com/dh/reservation_system/services/impl/DentistService.java
@@ -9,13 +9,14 @@ import com.dh.reservation_system.repositories.IDentistRepository;
 import com.dh.reservation_system.services.IDentistService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.transaction.Transactional;
+import org.apache.log4j.Logger;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class DentistService implements IDentistService {
-
+    private static final Logger logger = Logger.getLogger(DentistService.class);
     private final IDentistRepository dentistRepository;
     private final ObjectMapper objectMapper;
 
@@ -26,45 +27,65 @@ public class DentistService implements IDentistService {
 
     @Override
     public DentistResponseDto save(DentistRequestDto dentistDto) {
+        logger.info("Saving dentist: " + dentistDto.getName());
         Dentist dentist = mapToEntity(dentistDto);
         dentist = dentistRepository.save(dentist);
+        logger.info("Dentist saved: "+ dentist.getName());
         return mapToDto(dentist);
     }
 
     @Override
     public List<DentistResponseDto> findAll() {
-        return dentistRepository.findAll().stream()
+        logger.info("Finding all dentists");
+        List<DentistResponseDto> dentists = dentistRepository.findAll().stream()
                 .map(this::mapToDto)
                 .toList();
+        logger.info("Dentists found: "+ dentists.size());
+        return dentists;
     }
 
     @Override
     public DentistResponseDto findById(Long id) {
+        logger.info("Finding dentist by id: "+ id);
         Dentist dentist = dentistRepository.findById(id).orElseThrow(
-                () -> new NotFoundException("Dentist not found")
+                () -> {
+                    logger.error("Dentist with id " + id + " not found");
+                    return new NotFoundException("Dentist with id " + id + " not found");
+                }
         );
+        logger.info("Dentist found with id: "+ id);
         return mapToDto(dentist);
     }
 
     @Override
     public DentistResponseDto findByLicenseMedical(String licenseMedical) {
-        Dentist dentist =  dentistRepository.findByLicenseMedical(licenseMedical).orElseThrow(
-                () -> new NotFoundException("Dentist not found")
+        logger.info("Finding dentist by license medical: "+ licenseMedical);
+        Dentist dentist = dentistRepository.findByLicenseMedical(licenseMedical).orElseThrow(
+                () -> {
+                    logger.error("Dentist with license medical {} not found"+ licenseMedical);
+                    return new NotFoundException("Dentist with license medical " + licenseMedical + " not found");
+                }
         );
+        logger.info("Dentist found with license medical: "+ licenseMedical);
         return mapToDto(dentist);
     }
 
     @Override
     @Transactional
     public DentistResponseDto update(DentistRequestToUpdateDto dentistRequestToUpdateDto) {
+        logger.info("Updateing dentist with id: "+ dentistRequestToUpdateDto.getId());
         findById(dentistRequestToUpdateDto.getId());
-        return mapToDto(dentistRepository.save(mapToEntity(dentistRequestToUpdateDto)));
+        Dentist updatedDentist = dentistRepository.save(mapToEntity(dentistRequestToUpdateDto));
+        logger.info("Dentist with ID: " + dentistRequestToUpdateDto.getId() + " updated successfully");
+        return mapToDto(updatedDentist);
     }
 
     @Override
     public void delete(Long id) {
+        logger.info("Deleting dentist with id: " + id);
         findById(id);
         dentistRepository.deleteById(id);
+        logger.info("Dentist with ID: " + id +" deleted successfully");
     }
 
     private DentistResponseDto mapToDto(Dentist dentist) {
